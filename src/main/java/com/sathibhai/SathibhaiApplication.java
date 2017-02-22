bf1ac485b794cb8919fd97e37aee6e7d213dcbe6
Bipin Shrestha <bipinstha@Bipins-MacBook-Pro.local>
2017-02-21 22:53:08 -0500

    initial commit

---
+++ src/main/java/com/sathibhai/SathibhaiApplication.java
@@ -0,0 +1,12 @@
+package com.sathibhai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SathibhaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SathibhaiApplication.class, args);
+	}
+}
